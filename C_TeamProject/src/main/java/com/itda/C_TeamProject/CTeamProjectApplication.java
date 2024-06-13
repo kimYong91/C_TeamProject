68bb46a7fedf5cc7b159ab2747067f14bf6acfb9
KimYong91 <rladyd97@naver.com>
2024-06-13 15:45:05 +0900

    start

---
+++ C_TeamProject/src/main/java/com/itda/C_TeamProject/CTeamProjectApplication.java
@@ -0,0 +1,13 @@
+package com.itda.C_TeamProject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CTeamProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CTeamProjectApplication.class, args);
+	}
+
+}
---
+++ C_TeamProject/src/test/java/com/itda/C_TeamProject/CTeamProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.itda.C_TeamProject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CTeamProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
