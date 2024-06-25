78ad3ffd161e43aaaec74e38965989ceabf5a889
Kwang-min-lee1 <minwinn1@gmail.com>
2024-06-25 12:20:47 +0900

    기초대사량 정보 수정

--- C_TeamProject/src/main/java/com/itda/C_TeamProject/user/User.java
+++ C_TeamProject/src/main/java/com/itda/C_TeamProject/user/User.java
@@ -47,11 +47,12 @@ public class User {
 
         if (gender.equalsIgnoreCase("남")) {
             bmr = 88.362 + (13.397 * weight) + (4.799 * height) - (5.677 * age);
+
         } else if (gender.equalsIgnoreCase("여")) {
             bmr = 447.593 + (9.247 * weight) + (3.098 * height) - (4.330 * age);
         }
 
-        return bmr;
+        return Math.ceil(bmr);
     }
 
     public static int calculateAge(String birthDateString) {
